4928565eb6f709f04fc286237e058b9cda1d0965
pcmjames <pcmjames@gmail.com>
2017-07-20 16:35:59 +0800

    user-client返回值改为String

--- userclient/src/main/java/com/example/demo/controller/UserController.java
+++ userclient/src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.model.User;
 import com.example.demo.remote.UserRemote;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +13,7 @@ import java.util.List;
 /**
  * Created by pcm on 2017/7/19.
  */
+
 @RestController
 public class UserController
 {
@@ -17,17 +21,40 @@ public class UserController
     UserRemote userRemote;
 
     @GetMapping("/user")
-    public List findUser(){
-        return userRemote.findUser();
+    public String findUser(){
+        String jsonStr;
+        List<User> users = userRemote.findUser();
+        if(users == null || users.isEmpty()){
+            jsonStr = "服务出错了！";
+        }else{
+            jsonStr = JSON.toJSONString(users);
+        }
+        return jsonStr;
     }
 
+
     @GetMapping("/user/{id}")
-    public User findUserById(@PathVariable("id") Integer userId){
-        return userRemote.findUserById(userId);
+    public String findUserById(@PathVariable("id") Integer userId){
+        String jsonStr;
+        User user = userRemote.findUserById(userId);
+        if(user == null){
+            jsonStr = "未找到该用户！";
+        }else{
+            jsonStr = JSON.toJSONString(user);
+        }
+
+        return jsonStr;
     }
 
     @PostMapping("/addUser")
-    public User addUser(@RequestBody User user){
-        return userRemote.addUser(user);
+    public String addUser(@RequestBody User user){
+        String jsonStr;
+        try{
+            User user1 = userRemote.addUser(user);
+            jsonStr = JSON.toJSONString(user1);
+        }catch (Exception e){
+            jsonStr = "添加用户失败！";
+        }
+        return jsonStr;
     }
 }
