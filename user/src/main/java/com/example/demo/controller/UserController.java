47bbdaf3be06904f7610d5cf2348e0b2f7e2b279
pcmjames <pcmjames@gmail.com>
2017-07-20 12:26:27 +0800

    user-server配置刷新

--- user/src/main/java/com/example/demo/controller/UserController.java
+++ user/src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.demo.Service.UserService;
 import com.example.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 /**
  * Created by pcm on 2017/7/19.
  */
+@RefreshScope
 @RestController
 public class UserController
 {
