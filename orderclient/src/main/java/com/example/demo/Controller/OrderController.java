2f3802affe22ca1fa552b3ae4745fd44acc6779e
pcmjames <pcmjames@gmail.com>
2017-07-21 15:16:29 +0800

    order-client从配置中心读取配置文件

--- orderclient/src/main/java/com/example/demo/Controller/OrderController.java
+++ orderclient/src/main/java/com/example/demo/Controller/OrderController.java
@@ -6,6 +6,8 @@ import com.example.demo.model.NewOrder;
 import com.example.demo.model.Order;
 import com.example.demo.remote.OrderRemote;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,15 @@ import java.util.List;
 /**
  * Created by pcm on 2017/7/20.
  */
+@RefreshScope
 @RestController
 public class OrderController
 {
     @Autowired
     private OrderRemote orderRemote;
+
+    @Value("${hello}")
+    private String hello;
 
     @GetMapping("/findAllOrder")
     public String findAllOrder()
@@ -33,7 +39,7 @@ public class OrderController
             jsonStr = "服务出错了！";
         } else if (orders.isEmpty())
         {
-            jsonStr = "订单数据为空！";
+            jsonStr = hello+"  "+"订单数据为空！";
         } else
         {
             for (int i= 0; i<orders.size(); i++){
@@ -44,7 +50,7 @@ public class OrderController
                 newOrder.setCreatetime(orders.get(i).getCreatetime().toLocaleString());
                 newOrders.add(newOrder);
             }
-            jsonStr = JSON.toJSONString(newOrders);
+            jsonStr = hello+"  "+JSON.toJSONString(newOrders);
         }
         return jsonStr;
     }
@@ -60,7 +66,7 @@ public class OrderController
             jsonStr = "服务出错了！";
         } else if (orders.isEmpty())
         {
-            jsonStr = "没有找到订单数据！";
+            jsonStr = hello+"  "+"你还没有订单数据！";
         } else
         {
             for (int i= 0; i<orders.size(); i++){
@@ -71,7 +77,7 @@ public class OrderController
                 newOrder.setCreatetime(orders.get(i).getCreatetime().toLocaleString());
                 newOrders.add(newOrder);
             }
-            jsonStr = JSON.toJSONString(newOrders);
+            jsonStr = hello+"  "+ JSON.toJSONString(newOrders);
         }
         return jsonStr;
     }
