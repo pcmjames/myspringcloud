bc143f7327b642987523a36f3bab62120baa6ee9
pcmjames <pcmjames@gmail.com>
2017-07-21 09:58:16 +0800

    order-server注册druid

--- order/src/main/java/com/example/demo/OrderApplication.java
+++ order/src/main/java/com/example/demo/OrderApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@ServletComponentScan
 @EnableEurekaClient
 @MapperScan("com.example.demo.mapper")
 @SpringBootApplication
